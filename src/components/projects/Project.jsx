c48784fcbac357e19968546c890f99f8d3a1ff16
SoniRe <helloadityayo@gmail.com>
2024-01-12 14:55:21 +0530

    Projects Updated

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,5 +1,5 @@
 import "./project.css";
-import projectimage1 from "../../assets/project1.jpeg";
+import projectimage1 from "../../assets/project1.png";
 import projectimage2 from "../../assets/project2.png";
 import projectimage3 from "../../assets/project3.png";
 
@@ -11,25 +11,30 @@ const Project = () => {
 
       <div className="project__container">
         <div className="project1 project-ex">
-          <img src={projectimage3} alt="project-img" className="project__img" />
+          <img src={projectimage1} alt="project-img" className="project__img" />
           <div className="about__data project__data">
-            <h1 className="project__title">Youtube Clone</h1>
+            <h1 className="project__title">Youtube</h1>
             <p className="about__project">
-              YourTube, the ultimate video streaming app that brings
-              entertainment, education, and inspiration right to your
-              fingertips. With YourTube, you can explore a vast library of
-              videos, discover new content creators, and engage with a vibrant
-              community of viewers, all in one seamless platform.
+              🎥 Developed a top-notch video streaming platform with swift
+              loading times and an integrated live chat system. 🔗 Seamless API
+              polling facilitates real-time interactions. <br /> <br /> 🔍
+              Enhanced search performance with intelligent debouncing, tailored
+              API calls at less than 200ms keystroke intervals, and a strategic
+              Map data structure for Order of 1 time complexity. 💡 Result?
+              Lightning-fast suggestions with efficient caching. <br /> <br />{" "}
+              🌟 Elevated UX design by implementing N-Level Nested comments
+              using Recursive Component architecture. 🚀 Demonstrating
+              proficiency in system enhancement. 👩‍💻
             </p>
             <div className="project__details">
               <a
-                href="https://modern-yourtube.netlify.app/"
+                href="https://my-latest-youtube.netlify.app/"
                 className="project__link"
               >
                 Live Demo
               </a>
               <a
-                href="https://github.com/SoniRe/youtube-clone"
+                href="https://github.com/SoniRe/My-Youtube"
                 className="project__link"
               >
                 Github
@@ -38,24 +43,29 @@ const Project = () => {
           </div>
         </div>
         <div className="project2 project-rev">
-          <img src={projectimage1} alt="project-img" className="project__img" />
+          <img
+            src={projectimage2}
+            alt="project-img"
+            className="project__img2"
+          />
           <div className="about__data project__data">
-            <h1 className="project__title">Hikers</h1>
+            <h1 className="project__title">Netflix AI</h1>
             <p className="about__project">
-              Hikers is a web application for booking tours and hikes. It
-              provides users with an intuitive and seamless experience for
-              discovering, booking, and managing various outdoor activities.
-              This repository contains the source code for the Hikers web app.
+              🛠 Tech Stack: React, Redux, Firebase, Tailwind CSS. <br /> <br />{" "}
+              🧠 AI Magic: Gemini Pro for movie recommendations. <br /> <br />{" "}
+              🎥 Content Galore: TMDB API integration for diverse content.{" "}
+              <br /> <br />
+              🌐 Impact: Elevating user engagement with AI-driven suggestions.
             </p>
             <div className="project__details">
               <a
-                href="https://origin-hikers.netlify.app/"
+                href="https://netflix-googleai.netlify.app/"
                 className="project__link"
               >
                 Live Demo
               </a>
               <a
-                href="https://github.com/SoniRe/Hikers"
+                href="https://github.com/SoniRe/Netflix-AI"
                 className="project__link"
               >
                 Github
@@ -64,25 +74,37 @@ const Project = () => {
           </div>
         </div>
         <div className="project3 project-ex">
-          <img src={projectimage2} alt="project-img" className="project__img" />
+          <img src={projectimage3} alt="project-img" className="project__img" />
           <div className="about__data project__data">
-            <h1 className="project__title">Tindog</h1>
+            <h1 className="project__title">Foodiez</h1>
             <p className="about__project">
-              Tindog, the ultimate social networking app for pets, is here to
-              revolutionize the way your furry friend connects with others.
-              Whether you have a playful pup, a curious cat, or an adventurous
-              rabbit, Tindog is designed to bring pets and their owners together
-              in a fun and interactive way.
+              Foodiez a food delivery app built solely with React! 🍔📲 Key
+              Features: <br />
+              <br />
+              1️⃣ Config Driven UI: Tailored user experience through dynamic
+              configuration. <br />
+              <br />
+              2️⃣ Lightning-fast Performance: Thanks to React's efficient DOM
+              manipulation. <br />
+              <br />
+              3️⃣ Client Side Routing: Seamless navigation for a smooth user
+              journey.
+              <br />
+              <br /> 4️⃣ Redux Magic: Say goodbye to props drilling! Leveraged
+              Redux store for cleaner, more maintainable code. <br />
+              <br />
+              5️⃣ Custom Hooks FTW: Embracing Single Responsibility Principle
+              with custom hooks.
             </p>
             <div className="project__details">
               <a
-                href="https://sonire.github.io/tindog/"
+                href="https://fooodiezz.netlify.app/"
                 className="project__link"
               >
                 Live Demo
               </a>
               <a
-                href="https://github.com/SoniRe/tindog"
+                href="https://github.com/SoniRe/Foodiez"
                 className="project__link"
               >
                 Github
